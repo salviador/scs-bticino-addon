69defc02edc09a8f3e1dbb0c7e511085a2e9e07f
Michele <michelemancini2@tiscali.it>
2025-10-09 11:24:40 +0200

    1.0.9.36

--- scs-bticino/rootfs/app/main.py
+++ scs-bticino/rootfs/app/main.py
@@ -708,10 +708,10 @@ async def deviceReceiver_from_SCSbus(jqueqe):
                         bit1 = SCS.bitwise_and_bytes(trama[6], b'\x0F')
                         if bit1 in [b'\x02', b'\x00']:
                             device.Set_Modalita_Termostato(device.MODALITA.ESTATE)
-                            await scsmqtt.post_to_MQTT(f"/scsshield/device/{device_slug}/modalita_termostato_impostata", device.MODALITA.INVERNO.name)
+                            await scsmqtt.post_to_MQTT(f"/scsshield/device/{device_slug}/modalita_termostato_impostata", device.MODALITA.ESTATE.name)
                         elif bit1 in [b'\x03', b'\x01']:
                             device.Set_Modalita_Termostato(device.MODALITA.INVERNO)
-                            await scsmqtt.post_to_MQTT(f"/scsshield/device/{device_slug}/modalita_termostato_impostata", device.MODALITA.ESTATE.name)
+                            await scsmqtt.post_to_MQTT(f"/scsshield/device/{device_slug}/modalita_termostato_impostata", device.MODALITA.INVERNO.name)
                         elif trama[6] == b'\xFF':
                             device.Set_Modalita_Termostato(device.MODALITA.OFF)
                             await scsmqtt.post_to_MQTT(f"/scsshield/device/{device_slug}/modalita_termostato_impostata", device.MODALITA.OFF.name)
